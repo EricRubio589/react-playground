d30664081c341e76051f92b9d410574dc5c62cce
Eric Rubio <eric.rubio.services@gmail.com>
2019-08-22 23:56:54 -0500

    added API folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DemonymApp from './demonymapp/demonymApp'
 //import App from './App';
 //import HelloWorld from './state-drills/HelloWorld';
 //import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+// import RouletteGun from './state-drills/RouletteGun';
 import './index.css';
 
-ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+ReactDOM.render(<DemonymApp />, document.getElementById('root'));
