7c126c9aa8c147c7755deb40b2b1d2a2463aef3d
EricRubio589 <eric.rubio.services@gmail.com>
2019-11-04 06:29:01 -0600

    added lang-content

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DemonymApp from './demonymapp/demonymApp'
+//import DemonymApp from './demonymapp/demonymApp';
+import { BrowserRouter } from "react-router-dom";
 //import App from './App';
 //import HelloWorld from './state-drills/HelloWorld';
 //import Bomb from './state-drills/Bomb';
 // import RouletteGun from './state-drills/RouletteGun';
+import AppLang from './lang-context/AppLang';
+//import App from './paltrows-power-toes/App';
 import './index.css';
 
-ReactDOM.render(<DemonymApp />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    <AppLang />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
